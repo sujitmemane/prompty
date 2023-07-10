e2febff1f4f5903f81f133b93282c7ca16acbebd
“sujitmemane” <officialsujitmemane@gmail.comgit config --global user.name sujitmemanegit config --global user.email officialsujitmemane@gmail.com>
2023-07-10 17:25:09 +0530

    initial commit

---
+++ app/layout.jsx
@@ -0,0 +1,26 @@
+import '@styles/global.css'
+
+
+export const metadata={
+    title:'Prompty',
+    description:'Discover and Share Prompts'
+}
+
+ const RootLayout = ({children}) => {
+  return (
+    <html lang='en'>
+        <body>
+            <div className="main">
+                <div className="gradient" />
+            </div>
+            <main className="app">
+                {
+                    children
+                }
+            </main>
+        </body>
+    </html>
+  )
+}
+
+export default RootLayout;
