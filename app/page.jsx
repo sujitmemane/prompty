031fb52319449761e0b44379cac53cfa52ea0c2a
“sujitmemane” <officialsujitmemane@gmail.comgit config --global user.name sujitmemanegit config --global user.email officialsujitmemane@gmail.com>
2023-07-19 14:29:48 +0530

    Made all necessary components

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,19 @@
+import Feed from "@components/Feed"
 
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+            Discoever and Share
+        </h1>
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center font-bold text-4xl">AI Powered Prompts</span>
+        <p className="desc text-center">
+            Prompty is open-source AI prompting tool for modern world to discover, create and share creative prompts. 
+        </p>
+        <Feed/>
+    </section>
   )
 }
 
